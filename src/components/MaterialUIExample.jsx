b381e8470ccdebf70cba472c8cf9031496f4d57d
Patrick Costello <31824057+prcostello@users.noreply.github.com>
2023-04-20 17:11:10 -0300

    Update MaterialUIExample.jsx

--- src/components/MaterialUIExample.jsx
+++ src/components/MaterialUIExample.jsx
@@ -8,6 +8,7 @@ const MaterialUIExample = () => {
 
     return (
         <div style={{display: 'flex'}}>
+             {/* The Stack component is a Material UI component. It is a container (div) that can be styled. */}
             <Stack>
                 <Button variant="contained" color="primary">
                     Hello
@@ -16,6 +17,7 @@ const MaterialUIExample = () => {
                     World
                 </Button>
             </Stack>
+            {/* This is the above example done with plain HTML, note with React we can do in-line styling and avoid CSS*/}
             <div style={{display: 'flex', flexDirection: "column"}}>
                 <button>
                     Hello
