1f7c5aa0df29f574a839f08eb3b0c495f9049f53
DanielTStoyles <daniel_stoyles@live.ca>
2023-06-02 15:11:40 -0230

    Item name and price display functional and dynamic, search button added to GeComponent,

--- src/components/grand-exchange/GeComponent.jsx
+++ src/components/grand-exchange/GeComponent.jsx
@@ -1,13 +1,24 @@
 /** @format */
 
 import React, { useEffect, useState } from "react";
-import { Card, CardContent, Typography, TextField } from "@mui/material";
-import getItemData from "./../../repository/getItemData";
+import {
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
+import getItemData, { getItemsData } from "./../../repository/getItemData";
 
 const GeComponent = ({}) => {
   const [itemData, setItemData] = useState(null);
   const [itemName, setItemName] = useState("");
-  const [error, setError] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [itemId, setItemId] = useState("");
+
+  const handleSubmit = () => {
+    setItemName(inputValue);
+  };
 
   const componentStyle = {
     display: "flex",
@@ -29,9 +40,17 @@ const GeComponent = ({}) => {
   useEffect(() => {
     if (itemName !== "") {
       const fetchItemData = async (itemName) => {
-        const data = await getItemData(itemName);
-        setItemData(data);
-        console.log(data, "data log");
+        const items = await getItemsData();
+        const item = items.find((item) => item.name === itemName);
+        if (item) {
+          const data = await getItemData(item.id);
+          setItemId(item.id);
+
+          setItemData(data);
+          console.log(data, "data log");
+        } else {
+          console.log("item not found");
+        }
       };
 
       fetchItemData(itemName);
@@ -43,17 +62,20 @@ const GeComponent = ({}) => {
       <Card style={componentStyle}>
         <CardContent>
           <TextField
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
-            placeholder="Enter Item Id"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Enter Item Name"
           />
+          <Button onClick={handleSubmit} variant="contained">
+            Search
+          </Button>
 
           <Typography variant="h5" component="h2">
-            {itemData && itemData.item ? itemData.item.name : "Loading..."}
+            {itemData ? itemName : "Loading..."}
           </Typography>
           <Typography variant="body2" component="p">
-            {itemData && itemData.item
-              ? `Current Price: ${itemData.item.current.price}`
+            {itemData && itemData.data && itemData.data[itemId]
+              ? `High Price: ${itemData.data[itemId].high}, Low Price: ${itemData.data[itemId].low}`
               : "Loading..."}
           </Typography>
         </CardContent>
--- src/repository/getItemData.js
+++ src/repository/getItemData.js
@@ -2,7 +2,7 @@
 
 const getItemData = async (itemId) => {
   try {
-    const response = await fetch(`/api/item/${itemId}`);
+    const response = await fetch(`/api/items/${itemId}`);
     console.log(response);
     const data = await response.json();
 
@@ -18,4 +18,21 @@ const getItemData = async (itemId) => {
   }
 };
 
+const getItemsData = async () => {
+  try {
+    const response = await fetch("http://localhost:3001/api/items");
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`Error fetching items data: ${response.statusText}`);
+    }
+
+    return data;
+  } catch (error) {
+    console.error(`Error fetching items data: ${error.message}`);
+    return null;
+  }
+};
+
 export default getItemData;
+export { getItemsData };
