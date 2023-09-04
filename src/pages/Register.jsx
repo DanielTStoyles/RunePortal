91cbcd55f93c4285548027eddf393e8e2711d109
DanielTStoyles <daniel_stoyles@live.ca>
2023-09-04 17:52:22 -0230

    class components-->functional components

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,104 +1,97 @@
 /** @format */
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class RegisterPage extends Component {
-  state = {
-    credentials: { email: "", password: "", rsn: "" },
-  };
+const RegisterPage = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    this.setState((prevState) => ({
-      credentials: {
-        ...prevState.credentials,
-        [name]: value,
-      },
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [name]: value,
     }));
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log(this.state.credentials);
+    console.log(credentials);
   };
 
-  render() {
-    const { email, password, rsn } = this.state.credentials;
+  const { email, password, rsn } = credentials;
 
-    return (
-      <div
-        className="min-h-screen bg-[url(https://www.runescape.com/l=1/img/rsp777/bg2.jpg)] bg-repeat-y bg-center bg-top mx-auto 
+  return (
+    <div
+      className="min-h-screen bg-[url(https://www.runescape.com/l=1/img/rsp777/bg2.jpg)] bg-repeat-y bg-center bg-top mx-auto 
       overflow-y-hidden px-012 flex items-center justify-center bg-black py-12 px-4 sm:px-6 lg:px-8"
-      >
-        <div className="max-w-md w-full space-y-8">
-          <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-200">
-              Register for an account
-            </h2>
-          </div>
-          <form className="mt-8 space-y-6" onSubmit={this.handleSubmit}>
-            <input type="hidden" name="remember" value="true" />
-            <div className="rounded-md shadow-sm">
-              <div className="mb-2">
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-lime-600 focus:border-lime-600 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
-                  value={email}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className="mb-2">
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-lime-600 focus:border-lime-600 focus:z-10 sm:text-sm"
-                  placeholder="Password"
-                  value={password}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className="mb-2">
-                <label htmlFor="RSN" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="RSN"
-                  name="RSN"
-                  type="username"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-lime-600 focus:border-lime-600 focus:z-10 sm:text-sm"
-                  placeholder="RSN"
-                  value={rsn}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-lime-400 text-md font-medium rounded-md text-white bg-stone-600 hover:bg-stone-700 focus:outline-none focus:ring-2 focus:ring-lime-600"
-              >
-                Register
-              </button>
-            </div>
-          </form>
+    >
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-200">
+            Register for an account
+          </h2>
         </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm">
+            <div className="mb-2">
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-lime-600 focus:border-lime-600 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+                value={email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-2">
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-lime-600 focus:border-lime-600 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={password}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-2">
+              <label htmlFor="RSN" className="sr-only">
+                Password
+              </label>
+              <input
+                id="RSN"
+                name="rsn"
+                type="username"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-lime-600 focus:border-lime-600 focus:z-10 sm:text-sm"
+                placeholder="RSN"
+                value={rsn}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-lime-400 text-md font-medium rounded-md text-white bg-stone-600 hover:bg-stone-700 focus:outline-none focus:ring-2 focus:ring-lime-600"
+            >
+              Register
+            </button>
+          </div>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default RegisterPage;
