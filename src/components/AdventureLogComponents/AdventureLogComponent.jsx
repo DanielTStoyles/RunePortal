2c78182963679c7a31f9dc1e14833ab201c856ab
DanielTStoyles <daniel_stoyles@live.ca>
2023-12-03 15:26:42 -0330

    Remove comments describing location

--- src/components/AdventureLogComponents/AdventureLogComponent.jsx
+++ src/components/AdventureLogComponents/AdventureLogComponent.jsx
@@ -37,44 +37,30 @@ const AdventureLogBook = () => {
         className="bg-gray-700 shadow-lg rounded-lg overflow-hidden ml-64"
         style={{ width: "1000px" }}
       >
-        {/* Top two smaller frames */}
         <div className="flex items-center ">
           <div className=" items-center w-1/2 p-4 border-r border-gray-600">
-            {/* Left Top Frame Content */}
-            <AdventureLogProfileCard /> {/* ... */}
+            <AdventureLogProfileCard />
           </div>
           <div className="w-1/2 p-4">
-            {/* Right Top Frame Content */}
             <PlayerStatsDisplay playerSkillsData={playerSkillsData} />
-            {/* ... */}
           </div>
         </div>
 
-        {/* Middle two larger frames */}
         <div className="flex">
           <div className="w-1/2 p-4 border-r border-gray-600">
-            {/* Left Middle Frame Content */}
             <AdventureLogRecentEvents />
-            {/* ... */}
           </div>
           <div className="w-1/2 p-4">
-            {/* Right Middle Frame Content */}
             <AdventureLogFriendRecentEvents />
-
-            {/* ... */}
           </div>
         </div>
 
-        {/* Bottom two smaller frames */}
         <div className="flex">
           <div className="w-1/2 p-4 border-r border-gray-600">
-            {/* Left Bottom Frame Content */}
-            <AdventureLogQuestComponent /> {/* ... */}
+            <AdventureLogQuestComponent />
           </div>
           <div className="w-1/2 p-4">
-            {/* Right Bottom Frame Content */}
             <FriendsListComponent />
-            {/* ... */}
           </div>
         </div>
       </div>
